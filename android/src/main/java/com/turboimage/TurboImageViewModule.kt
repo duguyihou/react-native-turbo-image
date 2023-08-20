f30f8a1ce0854d819f4eb27bdc848b1332812bfd
duguyihou <duguyihou@gmail.com>
2023-08-20 13:17:18 +1000

    feat(clear cache): wip android

---
+++ android/src/main/java/com/turboimage/TurboImageViewModule.kt
@@ -0,0 +1,38 @@
+package com.turboimage
+
+import coil.Coil
+import coil.annotation.ExperimentalCoilApi
+import coil.request.CachePolicy
+import com.facebook.react.bridge.*
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class TurboImageViewModule(private val context: ReactApplicationContext): ReactContextBaseJavaModule(context) {
+  override fun getName(): String = REACT_CLASS
+  @ReactMethod
+  fun clearAllCache() {
+    val loader = Coil.imageLoader(context)
+    if(loader.defaults.memoryCachePolicy != CachePolicy.DISABLED) {
+      clearMemoryCache()
+    }
+    if (loader.defaults.diskCachePolicy != CachePolicy.DISABLED) {
+      clearDiskCache()
+    }
+  }
+
+  @ReactMethod
+  fun clearMemoryCache() {
+    Coil.imageLoader(context).memoryCache?.clear()
+  }
+
+  @OptIn(ExperimentalCoilApi::class)
+  @ReactMethod
+  fun clearDiskCache() {
+    Coil.imageLoader(context).diskCache?.clear()
+  }
+  companion object {
+    private const val REACT_CLASS = "RCTTurboImageViewModule"
+
+
+  }
+}
