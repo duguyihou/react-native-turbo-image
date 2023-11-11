e8c7b8e9fbd0517742ff22b93c602de9b8b67dbf
duguyihou <duguyihou@gmail.com>
2023-11-11 20:54:57 +1100

    chore: remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,13 @@
 import React, { forwardRef, memo } from 'react';
 import type {
-  TransformsStyle,
   StyleProp,
   ViewProps,
   AccessibilityProps,
-  ShadowStyleIOS,
-  FlexStyle,
   ImageResizeMode,
+  ImageStyle,
 } from 'react-native';
-import {
-  Platform,
-  UIManager,
-  requireNativeComponent,
-  NativeModules,
-} from 'react-native';
+import { requireNativeComponent, NativeModules } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-turbo-image' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-export interface ImageStyle extends FlexStyle, TransformsStyle, ShadowStyleIOS {
-  backfaceVisibility?: 'visible' | 'hidden';
-  borderBottomLeftRadius?: number;
-  borderBottomRightRadius?: number;
-  backgroundColor?: string;
-  borderColor?: string;
-  borderWidth?: number;
-  borderRadius?: number;
-  borderTopLeftRadius?: number;
-  borderTopRightRadius?: number;
-  overlayColor?: string;
-  opacity?: number;
-}
 export interface TurboImageProps extends AccessibilityProps, ViewProps {
   url: string;
   resizeMode?: ImageResizeMode;
@@ -46,31 +20,14 @@ export interface TurboImageProps extends AccessibilityProps, ViewProps {
     };
   }) => void;
   ref?: React.Ref<any>;
-  /**
-   *
-   * Style
-   */
   style?: StyleProp<ImageStyle>;
-
-  /**
-   * A unique identifier for this element to be used in UI Automation testing scripts.
-   */
   testID?: string;
-
-  /**
-   * Render children within the image.
-   */
   children?: React.ReactNode;
 }
 
 const ComponentName = 'TurboImageView';
 
-const TurboImageView =
-  UIManager.getViewManagerConfig(ComponentName) != null
-    ? requireNativeComponent<TurboImageProps>(ComponentName)
-    : () => {
-        throw new Error(LINKING_ERROR);
-      };
+const TurboImageView = requireNativeComponent<TurboImageProps>(ComponentName);
 
 const TurboImageBase = (
   props: TurboImageProps & { forwardedRef: React.Ref<any> }
