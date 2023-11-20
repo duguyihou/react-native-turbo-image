3f44380cd1404de0b9541bd27f3aa6e72716c790
duguyihou <duguyihou@gmail.com>
2023-11-20 21:55:07 +1100

    42 prefetch and clean cache methods (#47)
    * feat(iOS): wip
    
    * feat(iOS): add prefetch and clean cache methods

--- ios/TurboImage/TurboImageViewManager.swift
+++ ios/TurboImage/TurboImageViewManager.swift
@@ -11,3 +11,27 @@ class TurboImageViewManager: RCTViewManager {
     return false
   }
 }
+
+extension TurboImageViewManager {
+  @objc
+  func prefetch(_ urlArray: [String]) {
+    let urls =  urlArray.map { url in URL(string: url )}.compactMap{ $0 }
+    let prefetcher = ImagePrefetcher(urls: urls)
+    prefetcher.start()
+  }
+  
+  @objc
+  func clearAllCache() {
+    ImageCache.default.clearCache()
+  }
+  
+  @objc
+  func clearMemoryCache() {
+    ImageCache.default.clearMemoryCache()
+  }
+  
+  @objc
+  func clearDiskCache() {
+    ImageCache.default.clearDiskCache()
+  }
+}
