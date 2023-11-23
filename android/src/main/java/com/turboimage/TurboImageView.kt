0d3a71730f6a10c30d18ec8fbc3e0133f97acffb
duguyihou <duguyihou@gmail.com>
2023-11-23 21:06:57 +1100

    51 refactor android with coil (#52)
    * refactor: wip
    
    * refactor: working

--- android/src/main/java/com/turboimage/TurboImageView.kt
+++ android/src/main/java/com/turboimage/TurboImageView.kt
@@ -2,8 +2,13 @@ package com.turboimage
 
 import android.annotation.SuppressLint
 import androidx.appcompat.widget.AppCompatImageView
+import coil.drawable.CrossfadeDrawable
 
 import com.facebook.react.uimanager.ThemedReactContext
 
 @SuppressLint("ViewConstructor")
-class TurboImageView(reactContext: ThemedReactContext) : AppCompatImageView(reactContext)
+class TurboImageView(reactContext: ThemedReactContext) : AppCompatImageView(reactContext) {
+  var url: String? = null
+  var base64Placeholder: String? = null
+  var crossfade: Int = CrossfadeDrawable.DEFAULT_DURATION
+}
