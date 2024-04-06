f43c52e99657a0fd600c5d287347b107861dac5c
duguyihou <duguyihou@gmail.com>
2024-04-06 19:43:42 +1100

    chore: add margin (#88)

--- example/src/screens/GridScreen.tsx
+++ example/src/screens/GridScreen.tsx
@@ -3,7 +3,7 @@ import TurboImage from 'react-native-turbo-image';
 import React from 'react';
 import { blurhashString } from '../mockData';
 
-const size = Dimensions.get('window').width / 3;
+const size = Dimensions.get('window').width / 3 - 2;
 const GridScreen = () => {
   const imageURLs = Array.from(
     { length: 234 },
@@ -58,5 +58,6 @@ const styles = StyleSheet.create({
   card: {
     width: size,
     height: size,
+    margin: 1,
   },
 });
