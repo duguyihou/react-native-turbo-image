64f88d495b77f89a05c5cf893861d98b9f5f1b03
duguyihou <duguyihou@gmail.com>
2024-05-02 18:44:10 +1000

    refactor: ios and android (#160)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 import TurboImage from './TurboImage';
-export type { CachePolicy, Start, Success, Failure } from './types';
+export type { CachePolicy, Start, Success, Failure, ResizeMode } from './types';
 export default TurboImage;
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
+import type {
+  AccessibilityProps,
+  ViewProps,
+  StyleProp,
+  ImageStyle,
+  ColorValue,
+  NativeSyntheticEvent,
+} from 'react-native';
+
 export type ResizeMode = 'contain' | 'cover' | 'stretch' | 'center';
 export const resizeMode = {
   contain: 'contain',
@@ -14,12 +23,6 @@ export const cachePolicy = {
   dataCache: 'dataCache',
 } as const;
 
-export type TurboImageApi = {
-  prefetch: (sources: string[]) => Promise<void>;
-  clearMemoryCache: () => Promise<void>;
-  clearDiskCache: () => Promise<void>;
-};
-
 export type TaskState = 'running' | 'cancelled' | 'completed';
 
 export type Start = {
@@ -34,4 +37,28 @@ export type Success = {
 
 export type Failure = {
   error: string;
+};
+
+export interface TurboImageProps extends AccessibilityProps, ViewProps {
+  src: string;
+  style: StyleProp<ImageStyle>;
+  resizeMode?: ResizeMode;
+  showActivityIndicator?: boolean;
+  blurhash?: string;
+  fadeDuration?: number;
+  borderRadius?: number;
+  rounded?: boolean;
+  blur?: number;
+  monochrome?: number | ColorValue;
+  resize?: number[];
+  cachePolicy?: CachePolicy;
+  onStart?: (result: NativeSyntheticEvent<Start>) => void;
+  onSuccess?: (result: NativeSyntheticEvent<Success>) => void;
+  onFailure?: (result: NativeSyntheticEvent<Failure>) => void;
+}
+
+export type TurboImageApi = {
+  prefetch: (sources: string[]) => Promise<void>;
+  clearMemoryCache: () => Promise<void>;
+  clearDiskCache: () => Promise<void>;
 };
