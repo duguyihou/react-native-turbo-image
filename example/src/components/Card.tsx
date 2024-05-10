8ce54f3812f4b047c01b9cf4b1e11ad21335953f
duguyihou <duguyihou@gmail.com>
2024-05-11 07:51:44 +1000

    chore(example): update (#172)

--- example/src/components/Card.tsx
+++ example/src/components/Card.tsx
@@ -1,41 +1,17 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  type ColorValue,
-  type NativeSyntheticEvent,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import TurboImage, {
-  type CachePolicy,
-  type Failure,
-  type Success,
-  type Start,
-} from 'react-native-turbo-image';
+import TurboImage, { type TurboImageProps } from 'react-native-turbo-image';
 
-type Props = {
+interface Props extends Omit<TurboImageProps, 'style'> {
   size: number[];
   title?: string;
-  src: string;
-  cachePolicy?: CachePolicy;
-  priority?: number;
-  rounded?: boolean;
-  monochrome?: number | ColorValue;
-  borderRadius?: number;
-  blur?: number;
-  blurhash?: string;
-  resize?: number[];
-  showActivityIndicator?: boolean;
-  onStart?: (result: NativeSyntheticEvent<Start>) => void;
-  onSuccess?: (result: NativeSyntheticEvent<Success>) => void;
-  onFailure?: (result: NativeSyntheticEvent<Failure>) => void;
-};
+}
 const Card = ({ title, size, ...props }: Props) => {
   return (
     <View style={styles.card}>
       <TurboImage
-        style={[styles.image, { width: size[0], height: size[1] }]}
         {...props}
+        style={[styles.image, { width: size[0], height: size[1] }]}
       />
       {title && <Text style={styles.title}>{title}</Text>}
     </View>
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
 import TurboImage from './TurboImage';
-export type { CachePolicy, Start, Success, Failure, ResizeMode } from './types';
+export type {
+  CachePolicy,
+  Start,
+  Success,
+  Failure,
+  ResizeMode,
+  TurboImageProps,
+} from './types';
 export default TurboImage;
