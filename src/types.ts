d28ef85d0b6888700cc83200a17809d7ed804332
duguyihou <duguyihou@gmail.com>
2024-05-11 13:54:50 +1000

    chore: remove unused code

--- src/types.ts
+++ src/types.ts
@@ -8,20 +8,8 @@ import type {
 } from 'react-native';
 
 export type ResizeMode = 'contain' | 'cover' | 'stretch' | 'center';
-export const resizeMode = {
-  contain: 'contain',
-  cover: 'cover',
-  stretch: 'stretch',
-  center: 'center',
-} as const;
 
 export type CachePolicy = 'memory' | 'urlCache' | 'dataCache';
-
-export const cachePolicy = {
-  memory: 'memory',
-  urlCache: 'urlCache',
-  dataCache: 'dataCache',
-} as const;
 
 export type TaskState = 'running' | 'cancelled' | 'completed';
 
