63aed956e991f99871cc571b05e9249bf756528a
duguyihou <duguyihou@gmail.com>
2024-06-01 16:43:55 +1000

    refactor(svg, gif): android

--- example/src/data.ts
+++ example/src/data.ts
@@ -601,32 +601,41 @@ export const svgData = [
   {
     title: 'SVG',
     url: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Swift_logo.svg',
+    isSVG: true,
   },
 ];
 
 export const gifData = [
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505557/77ff05ac-c2e7-11e4-9a09-ce5b7995cad0.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505565/8aa02c90-c2e7-11e4-8127-71df010ca06d.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505571/a28a6e2e-c2e7-11e4-8161-9f39cc3bb8df.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505576/b785a8ac-c2e7-11e4-831a-666e2b064b95.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505579/c88c77ca-c2e7-11e4-88ad-d98c7360602d.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505595/def06c06-c2e7-11e4-9cdf-d37d28618af0.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505634/26e5dad2-c2e8-11e4-89c3-3c3a63110ac0.gif',
+    isGif: true,
   },
   {
     url: 'https://cloud.githubusercontent.com/assets/1567433/6505643/42eb3ee8-c2e8-11e4-8666-ac9c8e1dc9b5.gif',
+    isGif: true,
   },
 ];
