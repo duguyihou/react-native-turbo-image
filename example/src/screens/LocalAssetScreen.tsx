b28b72621b06ff9a01871b8d921b229f87288d5b
duguyihou <duguyihou@gmail.com>
2024-08-21 19:23:32 +1000

    fix: local svg asset in iOS

--- example/src/screens/LocalAssetScreen.tsx
+++ example/src/screens/LocalAssetScreen.tsx
@@ -3,10 +3,19 @@ import Card from '../components/Card';
 
 const LocalAssetScreen = () => {
   return (
-    <Card
-      source={require('../../assets/local.png')}
-      style={{ width: 200, height: 200 }}
-    />
+    <>
+      <Card
+        source={require('../../assets/local.png')}
+        style={{ width: 200, height: 200 }}
+        title="png"
+      />
+      <Card
+        source={require('../../assets/Swift_logo.svg')}
+        style={{ width: 190, height: 60 }}
+        format="svg"
+        title="svg"
+      />
+    </>
   );
 };
 
