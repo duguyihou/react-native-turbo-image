06d3767ca9b42f80baf156c1223152a0f9c95773
duguyihou <duguyihou@gmail.com>
2024-08-23 19:23:42 +1000

    chore: update example

--- example/src/screens/LocalAssetScreen.tsx
+++ example/src/screens/LocalAssetScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Card from '../components/Card';
+import { ScrollView } from 'react-native';
 
 const LocalAssetScreen = () => {
   return (
-    <>
+    <ScrollView>
       <Card
         source={require('../../assets/local.png')}
         style={{ width: 200, height: 200 }}
@@ -15,7 +16,19 @@ const LocalAssetScreen = () => {
         format="svg"
         title="svg"
       />
-    </>
+      <Card
+        source={require('../../assets/gif.gif')}
+        style={{ width: 300, height: 300 }}
+        format="gif"
+        title="gif"
+      />
+      <Card
+        source={require('../../assets/apng.png')}
+        style={{ width: 300, height: 300 }}
+        format="apng"
+        title="apng"
+      />
+    </ScrollView>
   );
 };
 
